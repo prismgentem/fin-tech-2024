25bc9973af951e8c9b63d45b50cad016754630e2
prismgentem <foksfire12345px@gmail.com>
2024-10-02 22:12:08 +0300

    fix

--- crud-kuda-go/src/test/java/org/example/crudkudago/integration/CategoryControllerTest.java
+++ crud-kuda-go/src/test/java/org/example/crudkudago/integration/CategoryControllerTest.java
@@ -1,4 +1,4 @@
-package org.example.crudkudago;
+package org.example.crudkudago.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.crudkudago.controller.CategoryControllerImpl;
